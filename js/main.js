21f88a52c7804551f99d3ed2148690be64b058f3
Juampa <juan.pa.pe.co.98@gmail.com>
2022-06-13 12:30:01 +0200

    El mapa se ve y es una simulación muy básica de como sería.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-
 var map = L.map('map')
+var popup = L.popup();
+
 
 map.setView([41.66, -4.72], 15);
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
-    }).addTo(map);
+}).addTo(map);
 
 L.control.scale().addTo(map);
 
 L.marker([41.66, -4.71], {
     draggable: true
 }).addTo(map);
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
