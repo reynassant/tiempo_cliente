147ef47052ed226ce77adf13ea3675816433c5a2
David <boni_tarancon@live.com>
2022-06-16 13:58:58 +0200

    No funciona aun pero guardo cambios en el nuevo logreg.js para esta tard
    Soy David

---
+++ js/logreg.js
@@ -0,0 +1,27 @@
+/* esto para el register */
+
+let password= document.input(id=pass);
+let repite_password= document.input(id=ubication);
+/* tambien comprobar username respecto a la base de datos*/
+
+if (password=repite_password) {
+
+	window.alert("Registro exitoso")
+};
+
+
+
+
+
+
+
+
+/* esto para el login */
+
+/* cuando compruebes el usuario y la contraseña en la base de datos buttononclick.redirect  */
+
+function redirect()
+    {
+   
+    window.location.href="https://index.html";
+    }
